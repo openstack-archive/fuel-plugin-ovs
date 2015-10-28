89a079ddbb18b47b5323b5883eec836f630e3f20
lingyu1 <ling.y.yu@intel.com>
2015-10-28 17:43:35 +0800

    Implement: Fuel install OVS with NSH/DPDK
    Todo lists:
    1. update dependencies.txt after RPM/debian packages are uploaded to official site
    
    Closes-Bug:#1507877
    Change-Id: Ie41e37d4387a0c03c9610a36b1f7009850564bf4
    Signed-off-by: lingyu1 <ling.y.yu@intel.com>

---
+++ deployment_scripts/puppet/manifests/ovs-install-compute.pp
@@ -0,0 +1,40 @@
+$fuel_settings = parseyaml(file('/etc/compute.yaml'))
+$ovs_version = "2.4.90-1"
+if $operatingsystem == 'Ubuntu' {
+        package { 'openvswitch-datapath-dkms':
+                ensure => "${ovs_version}",
+        }
+        package { 'openvswitch-common':
+                ensure => "${ovs_version}",
+        }
+        package { 'openvswitch-switch':
+                ensure => "${ovs_version}",
+                require => Package['openvswitch-common','openvswitch-datapath-dkms'],
+        }
+} elsif $operatingsystem == 'CentOS' {
+	if $fuel_settings['fuel-plugin-ovs']['use_dpdk'] {
+        	package { 'openvswitch':
+                	ensure => "2.4.90-1.el6",
+		}
+        	package { 'kmod-openvswitch':
+			ensure => "2.4.90-2.el6",
+		}
+        	package { 'dpdk':
+                	ensure => "2.1.0-6.el6",
+		}
+        	package { 'dpdk-tools':
+                	ensure => "2.1.0-6.el6",
+		}
+        	package { 'dpdk-devel':
+                	ensure => "2.1.0-6.el6",
+		}
+
+        } else {
+	        package { 'openvswitch':
+	                ensure =>"2.4.90-1",
+	        }
+        	package { 'kmod-openvswitch':
+                	ensure => "2.4.90-1.el6",
+		}
+	}
+}
