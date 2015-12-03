21ad4fe74b578cef0a30551ac91467417e7a3981
lingyu1 <ling.y.yu@intel.com>
2015-12-04 00:24:38 +0800

    Implement: Fuel install OVS with NSH/DPDK
    Update docker build
    
    Change-Id: I4fa8384947534f8b2bd27e3efde3a364d224578e
    Closes-Bug:#1507877
    Signed-off-by: lingyu1 <ling.y.yu@intel.com>

--- deployment_scripts/puppet/manifests/ovs-install-compute.pp
+++ deployment_scripts/puppet/manifests/ovs-install-compute.pp
@@ -1,39 +1,47 @@
 $fuel_settings = parseyaml(file('/etc/compute.yaml'))
 if $operatingsystem == 'Ubuntu' {
         if $fuel_settings['fuel-plugin-ovs']['use_dpdk'] {
-		exec { "wget dpdk package":
-		        command => "wget http://10.20.0.2:8080/plugins/fuel-plugin-ovs-0.5/repositories/ubuntu/dpdk-install.tar.gz",
-		        path   => "/usr/bin:/usr/sbin:/bin:/sbin",
-		}
-		exec { "unzip dpdk package":
-		        command => "tar -xvzf /etc/fuel/plugins/fuel-plugin-ovs-0.5/dpdk-install.tar.gz",
-		        path   => "/usr/bin:/usr/sbin:/bin:/sbin",
-		}
-		exec { "install dpdk package":
-		        command => "/etc/fuel/plugins/fuel-plugin-ovs-0.5/dpdk-install/dpdk-install.sh",
-		        path   => "/usr/bin:/usr/sbin:/bin:/sbin",
-		}
+                exec { "wget dpdk":
+                        command => "wget http://10.20.0.2:8080/plugins/fuel-plugin-ovs-0.5/repositories/ubuntu/dpdk-install/dpdk-install.sh",
+                        path   => "/usr/bin:/usr/sbin:/bin:/sbin",
+                }
+                exec { "install dpdk":
+                        command => "bash /etc/fuel/plugins/fuel-plugin-ovs-0.5/dpdk-install.sh",
+                        path   => "/usr/bin:/usr/sbin:/bin:/sbin",
+                }
                 package { 'openvswitch-datapath-dkms':
-                        ensure => "2.4.90-1",
+                        ensure => "2.4.91-1",
                 }
                 package { 'openvswitch-common':
-                        ensure => "2.4.90-1",
+                        ensure => "2.4.91-1",
                 }
                 package { 'openvswitch-switch':
-                        ensure => "2.4.90-1",
+                        ensure => "2.4.91-1",
                         require => Package['openvswitch-common','openvswitch-datapath-dkms'],
                 }
-	} else {
-                package { 'openvswitch-datapath-dkms':
-                        ensure => "2.4.90-1",
+        } else {
+                exec { "install openvswitch-datapath-dkms package":
+                        command => "wget http://10.20.0.2:8080/plugins/fuel-plugin-ovs-0.5/repositories/ubuntu/openvswitch-datapath-dkms_2.4.90-1_all.deb;sudo dpkg -i openvswitch-datapath-dkms_2.4.90-1_all.deb",
+                        path   => "/usr/bin:/usr/sbin:/bin:/sbin",
                 }
-                package { 'openvswitch-common':
-                        ensure => "2.4.90-1",
+                exec { "install openvswitch-common package":
+                        command => "wget http://10.20.0.2:8080/plugins/fuel-plugin-ovs-0.5/repositories/ubuntu/openvswitch-common_2.4.90-1_amd64.deb;sudo dpkg -i openvswitch-common_2.4.90-1_amd64.deb",
+                        path   => "/usr/bin:/usr/sbin:/bin:/sbin",
                 }
-                package { 'openvswitch-switch':
-                        ensure => "2.4.90-1",
-                        require => Package['openvswitch-common','openvswitch-datapath-dkms'],
+                exec { "install openvswitch-switch package":
+                        command => "wget http://10.20.0.2:8080/plugins/fuel-plugin-ovs-0.5/repositories/ubuntu/openvswitch-switch_2.4.90-1_amd64.deb;sudo dpkg -i openvswitch-switch_2.4.90-1_amd64.deb",
+                        path   => "/usr/bin:/usr/sbin:/bin:/sbin",
                 }
-	}
+        }
+        if $fuel_settings['fuel-plugin-ovs']['use_dppd'] {
+                exec { "wget dppd":
+                        command => "wget http://10.20.0.2:8080/plugins/fuel-plugin-ovs-0.5/repositories/ubuntu/dppd-install/dppd-install.sh",
+                        path   => "/usr/bin:/usr/sbin:/bin:/sbin",
+                }
+                exec { "install dppd":
+                        command => "bash /etc/fuel/plugins/fuel-plugin-ovs-0.5/dppd-install.sh",
+                        path   => "/usr/bin:/usr/sbin:/bin:/sbin",
+                }
+        }
 } elsif $operatingsystem == 'CentOS' {
 }
--- ovs-nsh/build-ovs-nsh.sh
+++ ovs-nsh/build-ovs-nsh.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-cd /openvswitch;./boot.sh;./configure;make dist;tar -xzf openvswitch-2.4.90.tar.gz
-cd /openvswitch/openvswitch-2.4.90;dpkg-checkbuilddeps;DEB_BUILD_OPTIONS='parallel=8 nocheck' fakeroot debian/rules binary
+OVS_COMMIT=121daded51b9798fe3722824b27a05c16806cbd1
+PATCHES="060679 060680 060681 060682 060683 060684 060685"
+URL_OVS=https://github.com/openvswitch/ovs.git
+
+git clone ${URL_OVS} openvswitch
+cd openvswitch
+git checkout ${OVS_COMMIT} -b development
+for patch in ${PATCHES}
+do
+        patch -p1 < /patches/${patch}.patch
+done
+
+./boot.sh;./configure;make dist;tar -xzf openvswitch-2.4.90.tar.gz
+cd openvswitch-2.4.90;dpkg-checkbuilddeps;DEB_BUILD_OPTIONS='parallel=8 nocheck' fakeroot debian/rules binary
