28cb9b73c8576c361c372ee15be5218df84fe89f
lingyu1 <ling.y.yu@intel.com>
2015-12-04 00:24:38 +0800

    Implement: Fuel install OVS with NSH/DPDK
    Update docker build
    
    Change-Id: I4fa8384947534f8b2bd27e3efde3a364d224578e
    Closes-Bug:#1507877
    Signed-off-by: lingyu1 <ling.y.yu@intel.com>

--- deployment_scripts/puppet/manifests/ovs-install-primary-controller.pp
+++ deployment_scripts/puppet/manifests/ovs-install-primary-controller.pp
@@ -14,25 +14,27 @@ if $operatingsystem == 'Ubuntu' {
 		        path   => "/usr/bin:/usr/sbin:/bin:/sbin",
 		}
                 package { 'openvswitch-datapath-dkms':
-                        ensure => "2.4.90-1",
+                        ensure => "2.4.91-1",
                 }
                 package { 'openvswitch-common':
-                        ensure => "2.4.90-1",
+                        ensure => "2.4.91-1",
                 }
                 package { 'openvswitch-switch':
-                        ensure => "2.4.90-1",
+                        ensure => "2.4.91-1",
                         require => Package['openvswitch-common','openvswitch-datapath-dkms'],
                 }
 	} else {
-                package { 'openvswitch-datapath-dkms':
-                        ensure => "2.4.90-1",
+                exec { "install openvswitch-datapath-dkms package":
+                        command => "wget http://10.20.0.2:8080/plugins/fuel-plugin-ovs-0.5/repositories/ubuntu/openvswitch-datapath-dkms_2.4.90-1_all.deb;sudo dpkg -i openvswitch-datapath-dkms_2.4.90-1_all.deb",
+                        path   => "/usr/bin:/usr/sbin:/bin:/sbin",
                 }
-                package { 'openvswitch-common':
-                        ensure => "2.4.90-1",
+                exec { "install openvswitch-common package":
+                        command => "wget http://10.20.0.2:8080/plugins/fuel-plugin-ovs-0.5/repositories/ubuntu/openvswitch-common_2.4.90-1_amd64.deb;sudo dpkg -i openvswitch-common_2.4.90-1_amd64.deb",
+                        path   => "/usr/bin:/usr/sbin:/bin:/sbin",
                 }
-                package { 'openvswitch-switch':
-                        ensure => "2.4.90-1",
-                        require => Package['openvswitch-common','openvswitch-datapath-dkms'],
+                exec { "install openvswitch-switch package":
+                        command => "wget http://10.20.0.2:8080/plugins/fuel-plugin-ovs-0.5/repositories/ubuntu/openvswitch-switch_2.4.90-1_amd64.deb;sudo dpkg -i openvswitch-switch_2.4.90-1_amd64.deb",
+                        path   => "/usr/bin:/usr/sbin:/bin:/sbin",
                 }
 	}
 } elsif $operatingsystem == 'CentOS' {
