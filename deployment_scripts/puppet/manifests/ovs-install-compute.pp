fffe4bfecfc9328a17b67f0810d01adc3806bc27
lingyu1 <ling.y.yu@intel.com>
2015-12-22 21:38:45 +0800

    Clean up code Implement: Fuel install OVS with NSH/DPDK
    Change-Id: I3fe071eb23f3f425e183725440c5a041f8b8cfae
    Closes-Bug:#1507877
    Signed-off-by: lingyu1 <ling.y.yu@intel.com>

--- deployment_scripts/puppet/manifests/ovs-install-compute.pp
+++ deployment_scripts/puppet/manifests/ovs-install-compute.pp
@@ -3,27 +3,7 @@ if $operatingsystem == 'Ubuntu' {
         if $fuel_settings['fuel-plugin-ovs']['use_dpdk'] {
                 $NICS = $fuel_settings['fuel-plugin-ovs']['dpdk-bind-nic']
                 exec { "install dpdk":
-                        command => "wget http://10.20.0.2:8080/plugins/fuel-plugin-ovs-0.5/repositories/ubuntu/dpdk/dpdk-install.sh; bash ./dpdk-install.sh",
-                        path   => "/usr/bin:/usr/sbin:/bin:/sbin",
-                } ->
-                exec {'dpdk-init':
-                command => "wget http://10.20.0.2:8080/plugins/fuel-plugin-ovs-0.5/repositories/ubuntu/dpdk/dpdk.init -O /etc/init.d/dpdk;chmod +x /etc/init.d/dpdk",
-                path   => "/usr/bin:/usr/sbin:/bin:/sbin",
-                } ->
-                exec {'dpdk-conf':
-                command => "wget http://10.20.0.2:8080/plugins/fuel-plugin-ovs-0.5/repositories/ubuntu/dpdk/dpdk.conf -O /etc/default/dpdk.conf",
-                path   => "/usr/bin:/usr/sbin:/bin:/sbin",
-                } ->
-                exec {'adapt_conf_file':
-                command => "sed 's/DPDK_NIC_MAPPINGS=.*/DPDK_NIC_MAPPINGS=${NICS}/' -i /etc/default/dpdk.conf",
-                path   => "/usr/bin:/usr/sbin:/bin:/sbin",
-                } ->
-                exec {'dpdk start':
-                command => "bash /etc/init.d/dpdk start",
-                path   => "/usr/bin:/usr/sbin:/bin:/sbin",
-                } ->
-                exec { "install ovs with dpdk":
-                        command => "wget http://10.20.0.2:8080/plugins/fuel-plugin-ovs-0.5/repositories/ubuntu/dpdk/ovs-dpdk-install.sh; bash ./ovs-dpdk-install.sh",
+                        command => "wget http://10.20.0.2:8080/plugins/fuel-plugin-ovs-0.5/repositories/ubuntu/dpdk/dpdk-install.sh; bash ./dpdk-install.sh $NICS",
                         path   => "/usr/bin:/usr/sbin:/bin:/sbin",
                 }
         } else {
--- repositories/ubuntu/dpdk/dpdk-install.sh
+++ repositories/ubuntu/dpdk/dpdk-install.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
-set -eux
 
+NICS=$1
 INSTALL_HOME=/usr/share/dpdk/
 sudo rm -rf $INSTALL_HOME ; mkdir -p $INSTALL_HOME
 cd $INSTALL_HOME
 wget http://10.20.0.2:8080/plugins/fuel-plugin-ovs-0.5/repositories/ubuntu/dpdk/dpdk-2.1.0.tar.gz;
 tar -xvzf dpdk-2.1.0.tar.gz
 cd $INSTALL_HOME/dpdk-2.1.0; make install T=x86_64-native-linuxapp-gcc
-
+wget http://10.20.0.2:8080/plugins/fuel-plugin-ovs-0.5/repositories/ubuntu/dpdk/dpdk.init -O /etc/init.d/dpdk
+chmod +x /etc/init.d/dpdk
+wget http://10.20.0.2:8080/plugins/fuel-plugin-ovs-0.5/repositories/ubuntu/dpdk/dpdk.conf -O /etc/default/dpdk.conf
+sed "s/DPDK_NIC_MAPPINGS=.*/DPDK_NIC_MAPPINGS=${NICS}/" -i /etc/default/dpdk.conf
+sudo service dpdk start
+INSTALL_HOME=/usr/share/ovs-dpdk/
+sudo rm -rf $INSTALL_HOME ; mkdir -p $INSTALL_HOME
+cd $INSTALL_HOME
+wget http://10.20.0.2:8080/plugins/fuel-plugin-ovs-0.5/repositories/ubuntu/dpdk/openvswitch-datapath-dkms_2.4.90-1_all.deb
+sudo dpkg -i openvswitch-datapath-dkms_2.4.90-1_all.deb
+wget http://10.20.0.2:8080/plugins/fuel-plugin-ovs-0.5/repositories/ubuntu/dpdk/openvswitch-common_2.4.90-1_amd64.deb
+sudo dpkg -i openvswitch-common_2.4.90-1_amd64.deb
+wget http://10.20.0.2:8080/plugins/fuel-plugin-ovs-0.5/repositories/ubuntu/dpdk/openvswitch-switch_2.4.90-1_amd64.deb
+sudo dpkg -i openvswitch-switch_2.4.90-1_amd64.deb
