bca561ecd3121dcac0cb44f1b49092ab701a2835
Guo Ruijing <ruijing.guo@intel.com>
2016-04-28 16:36:48 +0800

    support fuel 9.0
    Change-Id: Ib30f429f7db8cc3503f94a4e6d4505ca7e0f542e
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- deployment_scripts/puppet/manifests/ovs-install-compute.pp
+++ deployment_scripts/puppet/manifests/ovs-install-compute.pp
@@ -1,16 +1,21 @@
-$fuel_settings = parseyaml(file('/etc/compute.yaml'))
+$fuel_settings = parseyaml(file('/etc/astute.yaml'))
 if $operatingsystem == 'Ubuntu' {
-    if $fuel_settings['fuel-plugin-ovs']['support_dpdk'] {
-        $packages='openvswitch-datapath-dkms_2.4.90.dpdk-1 openvswitch-common_2.4.90.dpdk-1 openvswitch-switch_2.4.90.dpdk-1'
+    if $fuel_settings['fuel-plugin-ovs']['support_nsh'] and
+       $fuel_settings['fuel-plugin-ovs']['support_dpdk'] {
+        exec { 'install ovs/nsh-dpdk':
+            command => '/usr/bin/apt-get -y --force-yes install openvswitch-datapath-dkms_2.4.90.nshdpdk-1 openvswitch-common_2.4.90.nshdpdk-1 openvswitch-switch_2.4.90.nshdpdk-1'
+        }
     }
-    if $fuel_settings['fuel-plugin-ovs']['support_nsh'] {
-        $packages='openvswitch-datapath-dkms_2.4.90.nsh-1 openvswitch-common_2.4.90.nsh-1  openvswitch-switch_2.4.90.nsh-1'
+    elsif $fuel_settings['fuel-plugin-ovs']['support_dpdk'] {
+        exec { 'install ovs/dpdk':
+            command => '/usr/bin/apt-get -y --force-yes install openvswitch-datapath-dkms_2.4.90.dpdk-1 openvswitch-common_2.4.90.dpdk-1 openvswitch-switch_2.4.90.dpdk-1'
+        }
     }
-    if $fuel_settings['fuel-plugin-ovs']['support_nsh_dpdk'] {
-        $packages='openvswitch-datapath-dkms_2.4.90.nshdpdk-1 openvswitch-common_2.4.90.nshdpdk-1 openvswitch-switch_2.4.90.nshdpdk-1'
+    elsif $fuel_settings['fuel-plugin-ovs']['support_nsh'] {
+        exec { 'install ovs/nsh':
+            command => '/usr/bin/apt-get -y --force-yes install openvswitch-datapath-dkms_2.4.90.nsh-1 openvswitch-common_2.4.90.nsh-1  openvswitch-switch_2.4.90.nsh-1'
+        }
     }
-    exec { 'ovs install':
-        command => '/usr/bin/apt-get -y --force-yes install $packages'
-    }
+
 } elsif $operatingsystem == 'CentOS' {
 }
