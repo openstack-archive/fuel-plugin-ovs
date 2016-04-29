a7d0e546afd2b0379eb0ead9ad818bdb7bcbe9c9
Guo Ruijing <ruijing.guo@intel.com>
2016-04-29 15:41:15 +0800

    Prevent build temp files in host
    Change-Id: I97c6f1c17b7a9655db7a79bda4802eaaf9d6938c
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- vagrant/build_fuel_plugin.sh
+++ vagrant/build_fuel_plugin.sh
@@ -9,4 +9,6 @@ sudo apt-get install ruby -y
 sudo gem install rubygems-update
 sudo gem install fpm
 sudo apt-get install docker.io -y
-cd /fuel-plugin; fpb --debug --build .
+cp -r /fuel-plugin /home/vagrant
+cd /home/vagrant/fuel-plugin; fpb --debug --build .
+cp /home/vagrant/fuel-plugin/*.rpm /vagrant
