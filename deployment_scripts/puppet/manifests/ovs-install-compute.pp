defbc9c41db7d3d97388d67a5664d051f7298874
Guo Ruijing <ruijing.guo@intel.com>
2016-05-20 14:58:14 +0800

    Do not merge: refactor scripts and update NSH patch
    Change-Id: Ifc76e9bf2f83f904e765e6e8216ed98910642bae
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- deployment_scripts/puppet/manifests/ovs-install-compute.pp
+++ deployment_scripts/puppet/manifests/ovs-install-compute.pp
@@ -1,21 +1,11 @@
 $fuel_settings = parseyaml(file('/etc/astute.yaml'))
+$master_ip = $::fuel_settings['master_ip']
+$support_nsh = $::fuel_settings['fuel-plugin-ovs']['support_nsh']
+$support_dpdk = $::fuel_settings['fuel-plugin-ovs']['support_dpdk']
 if $operatingsystem == 'Ubuntu' {
-    if $fuel_settings['fuel-plugin-ovs']['support_nsh'] and
-       $fuel_settings['fuel-plugin-ovs']['support_dpdk'] {
-        exec { 'install ovs/nsh-dpdk':
-            command => '/usr/bin/curl http://10.20.0.2:8080/plugins/fuel-plugin-ovs-0.9/repositories/ubuntu/install.sh | /bin/bash -s nshdpdk'
-        }
+    exec { 'install ovs':
+        command => 'curl http://$master_ip:8080/plugins/fuel-plugin-ovs-0.9/deployment_scripts/install.sh | bash -s $master_ip $support_nsh $support_dpdk',
+        path   => "/usr/bin:/usr/sbin:/bin:/sbin";
     }
-    elsif $fuel_settings['fuel-plugin-ovs']['support_dpdk'] {
-        exec { 'install ovs/dpdk':
-            command => '/usr/bin/curl http://10.20.0.2:8080/plugins/fuel-plugin-ovs-0.9/repositories/ubuntu/install.sh | /bin/bash -s dpdk'
-        }
-    }
-    elsif $fuel_settings['fuel-plugin-ovs']['support_nsh'] {
-        exec { 'install ovs/nsh':
-            command => '/usr/bin/curl http://10.20.0.2:8080/plugins/fuel-plugin-ovs-0.9/repositories/ubuntu/install.sh | /bin/bash -s nsh'
-        }
-    }
-
 } elsif $operatingsystem == 'CentOS' {
 }
