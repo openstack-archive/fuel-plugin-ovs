b3decd58841a1c8668930a8b84f5a328ab6e0992
Guo Ruijing <ruijing.guo@intel.com>
2016-05-20 14:58:14 +0800

    Do not merge: refactor scripts and update NSH patch
    Change-Id: Ifc76e9bf2f83f904e765e6e8216ed98910642bae
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

---
+++ deployment_scripts/install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -eux
+
+INSTALL_HOME=/tmp/ovs-nshdpdk
+rm -rf $INSTALL_HOME; mkdir -p $INSTALL_HOME
+cd $INSTALL_HOME
+
+host=$1
+nsh=$2
+dpdk=$3
+
+wget -r -nd -np http://$host:8080/plugins/fuel-plugin-ovs-0.9/ovs_package/ubuntu
+
+if [ $nsh = 'true' ]
+then
+    dpkg -i openvswitch-datapath-dkms_2.5.90-1_all.deb
+    dpkg -i openvswitch-common_2.5.90-1_amd64.deb
+    dpkg -i openvswitch-switch_2.5.90-1_amd64.deb
+    if [ $dpdk = 'true' ]
+        dpkg -i openvswitch-switch-dpdk_2.5.90-1_amd64.deb
+    fi
+fi
