8a47e35683a4cf692389ccd69c8e6cbe02ae6652
Ferenc Cserepkei <ferenc.cserepkei@ericsson.com>
2016-05-21 17:22:12 +0200

    fuel-plugin-ovs with Yi Y Yang NSH patches
     This change applies Yi's patchset
    https://github.com/yyang13/ovs_nsh_patches in fuel-plugin-ovs
    instead of the aging Pritesh NSH v8 patchset. Besides that upgrades
    Openvswith used from 2.4-rc1 to 2.5 LTS in the plugin hence the
    deployed OPNFV/Fuel Brahmaputra SR-2 cluster.
    
    Change-Id: Ia6a71664228979f56575d37ca9f1e28c0bb6cf46
    Signed-off-by: Ferenc Cserepkei <ferenc.cserepkei@ericsson.com>

--- ovs-nsh/build-ovs-nsh.sh
+++ ovs-nsh/build-ovs-nsh.sh
@@ -1,16 +1,38 @@
 #!/bin/bash
 
-OVS_COMMIT=121daded51b9798fe3722824b27a05c16806cbd1
-PATCHES="060679 060680 060681 060682 060683 060684 060685"
+OVS_COMMIT=7d433ae57ebb90cd68e8fa948a096f619ac4e2d8
 URL_OVS=https://github.com/openvswitch/ovs.git
 
+git config --global user.email "build@opnfv.org"
+git config --global user.name "build"
+
+cd $HOME
+
+rm -rf patches
+git clone https://github.com/yyang13/ovs_nsh_patches.git patches
+cd patches;
+PATCHES=$(echo *patch)
+
+cd $HOME
+
 git clone ${URL_OVS} openvswitch
+
 cd openvswitch
-git checkout ${OVS_COMMIT} -b development
+git checkout ${OVS_COMMIT} -b yyang13
 for patch in ${PATCHES}
 do
-        patch -p1 < /patches/${patch}.patch
+        git am ${HOME}/patches/${patch}
 done
 
-./boot.sh;./configure;make dist;tar -xzf openvswitch-2.4.90.tar.gz
-cd openvswitch-2.4.90;dpkg-checkbuilddeps;DEB_BUILD_OPTIONS='parallel=8 nocheck' fakeroot debian/rules binary
+./boot.sh
+./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var --with-linux=/lib/modules/$(uname -r)/build
+make dist
+OVSTBALL=$(echo openvswitch-*.tar.gz)
+OVSART='openvswitch-2.5.90'
+
+
+tar -xzf ${HOME}/openvswitch/$OVSTBALL
+cd $OVSART
+dpkg-checkbuilddeps
+DEB_BUILD_OPTIONS='parallel=8 nocheck' fakeroot debian/rules binary
+
--- repositories/ubuntu/dpdk/dpdk-install.sh
+++ repositories/ubuntu/dpdk/dpdk-install.sh
@@ -2,11 +2,13 @@
 
 NICS=$1
 INSTALL_HOME=/usr/share/dpdk/
+OVS_VER='2.5.90'
+DPDK_VER='2.2.0'
 rm -rf $INSTALL_HOME ; mkdir -p $INSTALL_HOME
 cd $INSTALL_HOME
-wget http://10.20.0.2:8080/plugins/fuel-plugin-ovs-0.5/repositories/ubuntu/dpdk/dpdk-2.1.0.bin.tar.gz
-tar xzvf dpdk-2.1.0.bin.tar.gz
-rm -rf dpdk-2.1.0.bin.tar.gz
+wget "http://10.20.0.2:8080/plugins/fuel-plugin-ovs-0.5/repositories/ubuntu/dpdk/dpdk-${DPDK_VER}.bin.tar.gz"
+tar xzvf "dpdk-${DPDK_VER}.bin.tar.gz"
+rm -rf dpdk-${DPDK_VER}.bin.tar.gz
 wget http://10.20.0.2:8080/plugins/fuel-plugin-ovs-0.5/repositories/ubuntu/dpdk/dpdk.init -O /etc/init.d/dpdk
 chmod +x /etc/init.d/dpdk
 wget http://10.20.0.2:8080/plugins/fuel-plugin-ovs-0.5/repositories/ubuntu/dpdk/dpdk.conf -O /etc/default/dpdk.conf
@@ -16,9 +18,9 @@ service dpdk start
 INSTALL_HOME=/usr/share/ovs-dpdk/
 rm -rf $INSTALL_HOME ; mkdir -p $INSTALL_HOME
 cd $INSTALL_HOME
-wget http://10.20.0.2:8080/plugins/fuel-plugin-ovs-0.5/repositories/ubuntu/dpdk/openvswitch-datapath-dkms_2.4.90-1_all.deb
-dpkg -i openvswitch-datapath-dkms_2.4.90-1_all.deb
-wget http://10.20.0.2:8080/plugins/fuel-plugin-ovs-0.5/repositories/ubuntu/dpdk/openvswitch-common_2.4.90-1_amd64.deb
-dpkg -i openvswitch-common_2.4.90-1_amd64.deb
-wget http://10.20.0.2:8080/plugins/fuel-plugin-ovs-0.5/repositories/ubuntu/dpdk/openvswitch-switch_2.4.90-1_amd64.deb
-dpkg -i openvswitch-switch_2.4.90-1_amd64.deb
+wget "http://10.20.0.2:8080/plugins/fuel-plugin-ovs-0.5/repositories/ubuntu/dpdk/openvswitch-datapath-dkms_${OVS_VER}-1_all.deb"
+dpkg -i "openvswitch-datapath-dkms_${OVS_VER}_all.deb"
+wget "http://10.20.0.2:8080/plugins/fuel-plugin-ovs-0.5/repositories/ubuntu/dpdk/openvswitch-common_${OVS_VER}-1_amd64.deb"
+dpkg -i "openvswitch-common_${OVS_VER}-1_amd64.deb"
+wget "http://10.20.0.2:8080/plugins/fuel-plugin-ovs-0.5/repositories/ubuntu/dpdk/openvswitch-switch_${OVS_VER}-1_amd64.deb"
+dpkg -i "openvswitch-switch_${OVS_VER}-1_amd64.deb"
