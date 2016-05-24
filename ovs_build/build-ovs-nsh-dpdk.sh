2bad5a25e9a5cabb6f5c767cef287fd1f2701bdd
Guo Ruijing <ruijing.guo@intel.com>
2016-05-24 07:59:45 +0000

    refactor scripts and update NSH patch
    Change-Id: Ifc76e9bf2f83f904e765e6e8216ed98910642bae
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- ovs_build/build-ovs-nsh-dpdk.sh
+++ ovs_build/build-ovs-nsh-dpdk.sh
@@ -1,43 +1,97 @@
 #!/bin/bash
 
-DPDK_VER=2.1.0
-BUILD_HOME=`pwd`/tmp
-
-export RTE_TARGET=x86_64-native-linuxapp-gcc
-export RTE_SDK=${BUILD_HOME}/dpdk-${DPDK_VER}
-export DPDK_BUILD=${RTE_SDK}/${RTE_TARGET}
-
-OVS_COMMIT=121daded51b9798fe3722824b27a05c16806cbd1
-PATCHES="060679 060680 060681 060682 060683 060684 060685"
+OVS_COMMIT=7d433ae57ebb90cd68e8fa948a096f619ac4e2d8
 URL_OVS=https://github.com/openvswitch/ovs.git
-URL_DPDK=http://dpdk.org/browse/dpdk/snapshot/dpdk-${DPDK_VER}.tar.gz
+OVS_VER=${OVS_VER:-2.5.90}
+BUILD_HOME=$HOME/nsh
+BUILD_DEST=${BUILD_DEST:-/deb}
 
-mkdir -p ${BUILD_HOME}
-cd ${BUILD_HOME}
-wget ${URL_DPDK}
-tar -xzvf dpdk-${DPDK_VER}.tar.gz
-cd dpdk-${DPDK_VER}
-sed -i -e 's/CONFIG_RTE_LIBRTE_VHOST=n/CONFIG_RTE_LIBRTE_VHOST=y/' \
-       -e 's/CONFIG_RTE_BUILD_COMBINE_LIBS=n/CONFIG_RTE_BUILD_COMBINE_LIBS=y/' \
-       -e 's/CONFIG_RTE_PKTMBUF_HEADROOM=128/CONFIG_RTE_PKTMBUF_HEADROOM=256/' \
-       config/common_linuxapp
-make install T=${RTE_TARGET}
+sudo apt-get build-dep openvswitch -y
+sudo apt-get -y install devscripts dpkg-dev git wget
+
+rm -rf ${BUILD_HOME}; mkdir -p ${BUILD_HOME}
 
 cd ${BUILD_HOME}
-git clone ${URL_OVS} openvswitch
-cd openvswitch
-git checkout ${OVS_COMMIT} -b development
+wget -c https://launchpad.net/ubuntu/+archive/primary/+files/dpdk_2.2.0-0ubuntu8.dsc
+wget -c https://launchpad.net/ubuntu/+archive/primary/+files/dpdk_2.2.0.orig.tar.gz
+wget -c https://launchpad.net/ubuntu/+archive/primary/+files/dpdk_2.2.0-0ubuntu8.debian.tar.xz
+dpkg-source -x dpdk_2.2.0-0ubuntu8.dsc
+
+# copy from debian/control
+sudo apt-get install -y debhelper \
+               dh-python \
+               dh-systemd \
+               doxygen  \
+               graphviz  \
+               inkscape  \
+               libcap-dev  \
+               libpcap-dev  \
+               libxen-dev  \
+               libxenstore3.0  \
+               python  \
+               python-sphinx  \
+               texlive-fonts-recommended  \
+               texlive-latex-extra
+
+cd dpdk-2.2.0; rm -rf debian/patches/;
+cat << EOF > debian/changelog
+dpdk (2.2.0-1) unstable; urgency=low
+   [ DPDK team]
+   * New upstream version
+EOF
+debian/rules build; fakeroot debian/rules binary
+cd ${BUILD_HOME}; sudo dpkg -i *.deb
+
+cd ${BUILD_HOME}
+wget -c https://launchpad.net/ubuntu/+archive/primary/+files/openvswitch-dpdk_2.4.0.orig.tar.gz
+wget -c https://launchpad.net/ubuntu/+archive/primary/+files/openvswitch-dpdk_2.4.0-0ubuntu1.dsc
+wget -c https://launchpad.net/ubuntu/+archive/primary/+files/openvswitch-dpdk_2.4.0-0ubuntu1.debian.tar.xz
+dpkg-source -x openvswitch-dpdk_2.4.0-0ubuntu1.dsc
+
+# copy from debian/control
+sudo apt-get intall -y autoconf \
+               automake \
+               bzip2 \
+               debhelper \
+               dh-autoreconf \
+               dh-systemd \
+               graphviz \
+               libdpdk-dev \
+               libfuse-dev \
+               libssl-dev \
+               libtool \
+               openssl \
+               procps \
+               python-all \
+               python-qt4 \
+               python-twisted-conch \
+               python-zopeinterface \
+               python-six
+
+git clone https://github.com/openvswitch/ovs.git
+cd ovs; git checkout ${OVS_COMMIT}
+DIR="$(dirname `readlink -f $0`)"
+PATCHES=$(cd patches; echo *patch; cd ..)
 for patch in ${PATCHES}
 do
-    patch -p1 < /ovs_build/patches/${patch}.patch
+    patch -p1 < {DIR}/patches/${patch}
 done
-export DEB_BUILD_OPTIONS='parallel=8 nocheck'
-sed -i "s/2.4.90/2.4.90.nshdpdk/g" debian/changelog
-sed -i "s/DATAPATH_CONFIGURE_OPTS.*=.*//" debian/rules
-sed -i "2iDATAPATH_CONFIGURE_OPTS='--with-dpdk=$DPDK_BUILD'" debian/rules
-sed -i "s/DATAPATH_CONFIGURE_OPTS.*=.*//" debian/rules.modules
-sed -i "2iDATAPATH_CONFIGURE_OPTS='--with-dpdk=$DPDK_BUILD'" debian/rules.modules
-debian/rules build
-fakeroot debian/rules binary
+cd ${BUILD_HOME}; tar czvf ovs.tar.gz ovs
+rm -rf openvswitch-dpdk-${OVS_VER}*
+cd openvswitch-dpdk-2.4.0; uupdate -v ${OVS_VER} ../ovs.tar.gz
+cd ../openvswitch-dpdk-${OVS_VER}
+sed -i "s/include\/rte_config.h/include\/dpdk\/rte_config.h/" acinclude.m4
+sed -i 's/DPDK_INCLUDE=.*/DPDK_INCLUDE=$RTE_SDK\/include\/dpdk/'  acinclude.m4
+autoreconf --install
+rm -rf debian/patches/ .git;
+cat << EOF > debian/changelog
+openvswitch-dpdk (${OVS_VER}-1) unstable; urgency=low
+   [ Open vSwitch team ]
+   * Support NSH
+EOF
+debian/rules build; fakeroot debian/rules binary
 
-cp ${BUILD_HOME}/*.deb /deb
+cd ${BUILD_HOME}/ovs
+debian/rules build; fakeroot debian/rules binary
+
+cp ${BUILD_HOME}/*.deb ${BUILD_DEST}
