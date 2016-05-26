ab4b3f7426b75901c0538c6fc1bd9ce142c3d359
Guo Ruijing <ruijing.guo@intel.com>
2016-05-26 08:11:13 +0000

    Fix install issues
    Change-Id: I6ad863bdb05e3ef7802395b8a4dcb737603bfb9a
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- deployment_scripts/install.sh
+++ deployment_scripts/install.sh
@@ -9,15 +9,30 @@ host=$1
 nsh=$2
 dpdk=$3
 
-wget -r -nd -np http://$host:8080/plugins/fuel-plugin-ovs-0.9/ovs_package/ubuntu
 
 if [ $nsh = 'true' ]
 then
+    wget  -r -np -nH --cut-dirs=3 http://$host:8080/plugins/fuel-plugin-ovs-0.9/ovs-nsh/
     dpkg -i openvswitch-datapath-dkms_2.5.90-1_all.deb
     dpkg -i openvswitch-common_2.5.90-1_amd64.deb
     dpkg -i openvswitch-switch_2.5.90-1_amd64.deb
+    dpkg -i python-openvswitch_2.5.90-1_all.deb
     if [ $dpdk = 'true' ]
     then
+        dpkg -i libxenstore3.0_4.4.2-0ubuntu0.14.04.5_amd64.deb
+        dpkg -i libdpdk0_2.2.0-1_amd64.deb
+        dpkg -i dpdk_2.2.0-1_amd64.deb
         dpkg -i openvswitch-switch-dpdk_2.5.90-1_amd64.deb
     fi
+elif [ $dpdk = 'true' ]
+then
+    wget  -r -np -nH --cut-dirs=3 http://$host:8080/plugins/fuel-plugin-ovs-0.9/ovs-dpdk/
+    dpkg -i libxenstore3.0_4.4.2-0ubuntu0.14.04.5_amd64.deb
+    dpkg -i libdpdk0_2.2.0-1_amd64.deb
+    dpkg -i dpdk_2.2.0-1_amd64.deb
+    dpkg -i openvswitch-datapath-dkms_2.5.1-1_all.deb
+    dpkg -i openvswitch-common_2.5.1-1_amd64.deb
+    dpkg -i openvswitch-switch_2.5.1-1_amd64.deb
+    dpkg -i python-openvswitch_2.5.1-1_all.deb
+    dpkg -i openvswitch-switch-dpdk_2.5.1-1_amd64.deb
 fi
--- deployment_scripts/puppet/manifests/ovs-install-compute.pp
+++ deployment_scripts/puppet/manifests/ovs-install-compute.pp
@@ -1,10 +1,10 @@
 $fuel_settings = parseyaml(file('/etc/astute.yaml'))
 $master_ip = $::fuel_settings['master_ip']
-$support_nsh = $::fuel_settings['fuel-plugin-ovs']['support_nsh']
-$support_dpdk = $::fuel_settings['fuel-plugin-ovs']['support_dpdk']
+$install_nsh = $::fuel_settings['fuel-plugin-ovs']['install_nsh']
+$install_dpdk = $::fuel_settings['fuel-plugin-ovs']['install_dpdk']
 if $operatingsystem == 'Ubuntu' {
     exec { 'install ovs_nsh_dpdk':
-        command => "curl http://${master_ip}:8080/plugins/fuel-plugin-ovs-0.9/deployment_scripts/install.sh | bash -s ${master_ip} ${support_nsh} ${support_dpdk}",
+        command => "curl http://${master_ip}:8080/plugins/fuel-plugin-ovs-0.9/deployment_scripts/install.sh | bash -s ${master_ip} ${install_nsh} ${install_dpdk}",
         path   => "/usr/bin:/usr/sbin:/bin:/sbin";
     }
 } elsif $operatingsystem == 'CentOS' {
