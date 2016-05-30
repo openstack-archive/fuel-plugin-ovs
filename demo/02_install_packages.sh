be8d32b422642e3945a7d92ceab7e72b337f9c44
Guo Ruijing <ruijing.guo@intel.com>
2016-05-30 16:08:35 -0700

    Update install packages script
    Change-Id: I79f8c3a4cf05b18d8020e8c96ce8f6b2d8b989bc
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- demo/02_install_packages.sh
+++ demo/02_install_packages.sh
@@ -5,10 +5,12 @@ fuel ALL = (root) NOPASSWD:ALL
 EOF
 
 sudo apt-get update -y
-sudo apt-get install openssh-server -y
-sudo apt-get autoremove gnome-settings-daemon-schemas -y
-sudo apt-get autoremove libmetacity-private0a metacity-common -y
-sudo apt-get install gnome-session gnome-panel gnome-settings-daemon metacity nautilus gnome-terminal -y
+sudo apt-get install openssh-server git -y
+
+#install vnc
+sudo apt-get install gnome-session gnome-panel gnome-settings-daemon metacity nautilus gnome-terminal vnc4server -y
+
+#install libvirt/qemu
 sudo apt-get install virt-manager libvirt-bin qemu-system -y
 sudo virsh net-destroy default
 sudo virsh net-undefine default
@@ -17,12 +19,13 @@ sudo service libvirt-bin restart
 sudo ifconfig virbr0 down
 sudo brctl delbr virbr0
 sudo apt-get install putty-tools -y
+
+#install ntp
 sudo service ntp stop
 sudo apt-get autoremove ntp -y
-sudo rm -rf /etc/ntp.conf.dpkg-old
-sudo rm -rf /etc/ntp.conf
 sudo apt-get install ntp -y --force-yes
-sudo bash -c 'cat << EOF >> /etc/ntp.conf
+cat << EOF | sudo tee /etc/ntp.conf
 server 127.127.1.0
 fudge  127.127.1.0 stratum 10
 EOF'
+sudo service ntp restart
