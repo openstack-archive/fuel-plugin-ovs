537bee446fd9fdac7347c476588c97b19391bf3d
Guo Ruijing <ruijing.guo@intel.com>
2016-06-07 00:20:52 +0000

    Update to DPDK 16.04 + OVS 2.5.90
    Change-Id: I6efd1fd3efb6f0c80313a7a1d9d8ac87486802f9
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- ovs_build/build-ovs-dpdk.sh
+++ ovs_build/build-ovs-dpdk.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-OVS_COMMIT=cd4764fdd8ce0aa0063525dad0e67f20b3bcf6e9
+set -eux
+
+OVS_COMMIT=7d8eadce4df70f563a0c0123c612f6117c8ff864
 URL_OVS=https://github.com/openvswitch/ovs.git
-OVS_VER=${OVS_VER:-2.5.1}
+OVS_VER=${OVS_VER:-2.5.90}
 BUILD_HOME=$HOME/dpdk
 BUILD_DEST=${BUILD_DEST:-/deb}
 
@@ -12,12 +14,14 @@ sudo apt-get build-dep openvswitch -y
 sudo apt-get -y install devscripts dpkg-dev git wget
 
 rm -rf ${BUILD_HOME}; mkdir -p ${BUILD_HOME}
+cp `dirname $0`/rules ${BUILD_HOME}
 
 cd ${BUILD_HOME}
 wget -c https://launchpad.net/ubuntu/+archive/primary/+files/dpdk_2.2.0-0ubuntu8.dsc
 wget -c https://launchpad.net/ubuntu/+archive/primary/+files/dpdk_2.2.0.orig.tar.gz
 wget -c https://launchpad.net/ubuntu/+archive/primary/+files/dpdk_2.2.0-0ubuntu8.debian.tar.xz
 dpkg-source -x dpdk_2.2.0-0ubuntu8.dsc
+wget -c http://fast.dpdk.org/rel/dpdk-16.04.tar.xz
 
 # copy from debian/control
 sudo apt-get install -y debhelper \
@@ -35,12 +39,15 @@ sudo apt-get install -y debhelper \
                texlive-fonts-recommended  \
                texlive-latex-extra
 
-cd dpdk-2.2.0; rm -rf debian/patches/;
+cd dpdk-2.2.0; rm -rf debian/patches/; uupdate -v 16.04 ../dpdk-16.04.tar.xz
+cd ${BUILD_HOME}/dpdk-16.04
 cat << EOF > debian/changelog
-dpdk (2.2.0-1) unstable; urgency=low
-   [ DPDK team]
-   * New upstream version
+dpdk (16.04-1) unstable; urgency=low
+  * DPDK 16.04
+ -- DPDK team <dev@dpdk.org>
 EOF
+mv debian/rules debian/rules.orig
+cp ${BUILD_HOME}/rules debian/rules
 debian/rules build; fakeroot debian/rules binary
 cd ${BUILD_HOME}; sudo dpkg -i *.deb
 apt-get download libxenstore3.0
@@ -77,14 +84,13 @@ cd ${BUILD_HOME}; tar czvf ovs.tar.gz ovs
 rm -rf openvswitch-dpdk-${OVS_VER}*
 cd openvswitch-dpdk-2.4.0; uupdate -v ${OVS_VER} ../ovs.tar.gz
 cd ../openvswitch-dpdk-${OVS_VER}
-sed -i "s/include\/rte_config.h/include\/dpdk\/rte_config.h/" acinclude.m4
-sed -i 's/DPDK_INCLUDE=.*/DPDK_INCLUDE=$RTE_SDK\/include\/dpdk/'  acinclude.m4
+#sed -i 's~DPDK_LIB_DIR=.*~DPDK_LIB_DIR="$with_dpdk/x86_64-linux-gnu/lib"~'  acinclude.m4
 autoreconf --install
 rm -rf debian/patches/ .git;
 cat << EOF > debian/changelog
 openvswitch-dpdk (${OVS_VER}-1) unstable; urgency=low
-   [ Open vSwitch team ]
-   * Open vSwitch Upstream
+  * New upstream version
+ -- Open vSwitch team <dev@openvswitch.org>
 EOF
 debian/rules build; fakeroot debian/rules binary
 
