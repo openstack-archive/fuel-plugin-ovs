be26f88e6e80c142a44418cd01c2f9b20b5e27a4
Guo Ruijing <ruijing.guo@intel.com>
2016-07-07 00:53:36 +0800

    Update fuel plugin builder
    Change-Id: Icfcc1364fc5bc17cdc15e323115e66f8089f95fc
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- vagrant/build_fuel_plugin.sh
+++ vagrant/build_fuel_plugin.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 sudo apt-get update -y
-sudo apt-get install createrepo rpm dpkg-dev -y
-sudo apt-get install python-setuptools -y
-sudo apt-get install python-pip -y
-sudo easy_install pip
-sudo pip install fuel-plugin-builder
-sudo apt-get install ruby -y
-sudo gem install rubygems-update
+sudo apt-get install -y ruby-dev rubygems-integration python-pip rpm createrepo dpkg-dev
 sudo gem install fpm
+sudo pip install fuel-plugin-builder
 sudo apt-get install docker.io -y
 cp -r /fuel-plugin /home/vagrant
 cd /home/vagrant/fuel-plugin; fpb --debug --build .
