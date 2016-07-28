5e5db9819ba0a321876f91563084505cf8e65faa
Guo Ruijing <ruijing.guo@intel.com>
2016-07-27 18:50:11 -0700

    Fix root permission issue
    Change-Id: I6d7cdab7505f5ab42c52457169dce5970f8bfac8
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- demo/02_install_packages.sh
+++ demo/02_install_packages.sh
@@ -17,6 +17,11 @@ sudo apt-get install virt-manager libvirt-bin qemu-system -y
 sudo virsh net-destroy default
 sudo virsh net-undefine default
 sudo rm -rf /etc/libvirt/qemu/networks/autostart/default.xml
+cat << EOF | sudo tee /etc/libvirt/qemu.conf
+user = "root"
+group = "root"
+EOF
+
 sudo service libvirt-bin restart
 sudo ifconfig virbr0 down
 sudo brctl delbr virbr0
