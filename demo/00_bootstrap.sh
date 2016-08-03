2c1a1f3b1818cd884965dcfea3ac7986d23047fe
Guo Ruijing <ruijing.guo@intel.com>
2016-08-03 14:51:49 -0400

    Add ssh server in demo
    Change-Id: Ibd574f852480fc834a8eadc79db21b980f3f4cdf
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- demo/00_bootstrap.sh
+++ demo/00_bootstrap.sh
@@ -6,8 +6,9 @@ Acquire::http::Proxy "$HTTP_PROXY";
 EOF
 
 sudo apt-get update -y
-sudo apt-get install -y git
+sudo apt-get install -y openssh-server git
 
+mv ~/.gitconfig ~/.gitconfig.bak
 git config --global http.proxy ${HTTP_PROXY}
 git clone https://github.com/openstack/fuel-plugin-ovs/
-git config --global --unset http.proxy
+mv ~/.gitconfig.bak ~/.gitconfig
