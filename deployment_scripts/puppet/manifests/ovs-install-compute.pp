0db45e140467be234b76a424577d25d88dddda7d
Michael Polenchuk <mpolenchuk@mirantis.com>
2016-08-26 16:09:56 +0300

    Allocate dpdk socket memory
    * allocate dpdk socket memory if configured
    * update main task to conform to F9.0+
    
    Change-Id: I520b92cfbda083ad5781c8bc89bde9b43dcbfa0f

--- deployment_scripts/puppet/manifests/ovs-install-compute.pp
+++ deployment_scripts/puppet/manifests/ovs-install-compute.pp
@@ -1,10 +1,16 @@
-$fuel_settings = parseyaml(file('/etc/astute.yaml'))
-$master_ip = $::fuel_settings['master_ip']
-$install_nsh = $::fuel_settings['fuel-plugin-ovs']['install_nsh']
-$install_dpdk = $::fuel_settings['fuel-plugin-ovs']['install_dpdk']
+# get options
+$master_ip      = hiera('master_ip')
+$ovs_settings   = hiera('fuel-plugin-ovs')
+$dpdk           = hiera('dpdk')
+
+$install_nsh    = $ovs_settings['install_nsh']
+$install_dpdk   = $ovs_settings['install_dpdk']
+$ovs_socket_mem = join(pick($dpdk['ovs_socket_mem'], []), ',')
+
+
 if $operatingsystem == 'Ubuntu' {
     exec { 'install ovs_nsh_dpdk':
-        command => "curl http://${master_ip}:8080/plugins/fuel-plugin-ovs-0.9/deployment_scripts/install.sh | bash -s ${master_ip} ${install_nsh} ${install_dpdk}",
+        command => "curl http://${master_ip}:8080/plugins/fuel-plugin-ovs-0.9/deployment_scripts/install.sh | bash -s ${master_ip} ${install_nsh} ${install_dpdk} ${ovs_socket_mem}",
         path   => "/usr/bin:/usr/sbin:/bin:/sbin";
     }
 } elsif $operatingsystem == 'CentOS' {
