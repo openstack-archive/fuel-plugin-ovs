cd2c69a55c2fdd3d5f68f7afc5bbc8640d2e4e8c
Michael Polenchuk <mpolenchuk@mirantis.com>
2016-08-26 16:09:56 +0300

    Mount hugepages for dpdk patently
    * add mount point for hugepages if exist
    * update main task to conform to F9.0+
    
    Change-Id: I520b92cfbda083ad5781c8bc89bde9b43dcbfa0f

--- deployment_scripts/puppet/manifests/ovs-install-compute.pp
+++ deployment_scripts/puppet/manifests/ovs-install-compute.pp
@@ -1,8 +1,29 @@
-$fuel_settings = parseyaml(file('/etc/astute.yaml'))
-$master_ip = $::fuel_settings['master_ip']
-$install_nsh = $::fuel_settings['fuel-plugin-ovs']['install_nsh']
-$install_dpdk = $::fuel_settings['fuel-plugin-ovs']['install_dpdk']
+# get options
+$master_ip    = hiera('master_ip')
+$ovs_settings = hiera('fuel-plugin-ovs')
+$hugepages    = hiera('hugepages', [])
+
+$install_nsh  = $ovs_settings['install_nsh']
+$install_dpdk = $ovs_settings['install_dpdk']
+
+
 if $operatingsystem == 'Ubuntu' {
+    unless empty($hugepages) {
+      $dev_hugepages = '/dev/hugepages'
+
+      file { $dev_hugepages:
+        ensure  => 'directory',
+      }
+
+      mount { $dev_hugepages:
+        ensure  => mounted,
+        device  => 'none'
+        fstype  => 'hugetlbfs',
+      }
+
+      File[$dev_hugepages] -> Mount[$dev_hugepages] -> Exec['install ovs_nsh_dpdk']
+    }
+
     exec { 'install ovs_nsh_dpdk':
         command => "curl http://${master_ip}:8080/plugins/fuel-plugin-ovs-0.9/deployment_scripts/install.sh | bash -s ${master_ip} ${install_nsh} ${install_dpdk}",
         path   => "/usr/bin:/usr/sbin:/bin:/sbin";
