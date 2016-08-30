7d90856a02fce1a379dcaf2c32dd9d5f6088c229
Nikolas Hermanns <nikolas.hermanns@ericsson.com>
2016-08-30 17:04:20 +0200

    Installing same ovs version on the controller
    This is a cherry pick of I454a9e6e577bca65a8ac54a55b44cc40b7f238fa
    This commit also brings in the bugfix for this, cherry-picked from
    Ifa961240d1ccd53e56b93d5afae7da0df26c1e34
    
    Change-Id: I43ae6e99ef5a6ab9b6d580f27390e42528c1e8ff
    Signed-off-by: Romanos Skiadas <rski@intracom-telecom.com>

---
+++ deployment_scripts/puppet/manifests/ovs-install.pp
@@ -0,0 +1,21 @@
+notice('MODULAR: ovs-install.pp')
+# get options
+$master_ip      = hiera('master_ip')
+$ovs_settings   = hiera('fuel-plugin-ovs')
+$dpdk           = hiera('dpdk')
+
+$install_nsh    = $ovs_settings['install_nsh']
+$install_dpdk   = $ovs_settings['install_dpdk']
+$install_on_controller = $ovs_settings['install_on_controller']
+$ovs_socket_mem = join(pick($dpdk['ovs_socket_mem'], []), ',')
+
+
+if $operatingsystem == 'Ubuntu' {
+  if (!roles_include(['primary-controller', 'controller'])) or $install_on_controller {
+      exec { 'install ovs_nsh_dpdk':
+          command => "curl http://${master_ip}:8080/plugins/fuel-plugin-ovs-0.9/deployment_scripts/install.sh | bash -s ${master_ip} ${install_nsh} ${install_dpdk} ${ovs_socket_mem}",
+          path    => '/usr/bin:/usr/sbin:/bin:/sbin',
+      }
+    }
+} elsif $operatingsystem == 'CentOS' {
+}
