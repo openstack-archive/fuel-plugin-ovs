996c928d4260b2190cc1b67d82ffed55ad958145
Guo Ruijing <ruijing.guo@intel.com>
2016-10-31 17:50:17 -0400

    Rebase to fuel 10.0
    Change-Id: I110a8b3626bcb3ac64c7f2b489755a9fbb4669ff
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- deployment_scripts/install.sh
+++ deployment_scripts/install.sh
@@ -12,7 +12,7 @@ dpdk_socket_mem=${4:-''}
 
 if [ $nsh = 'true' ]
 then
-    curl  http://$host:8080/plugins/fuel-plugin-ovs-0.9/repositories/ubuntu/ovs-nsh-dpdk.tar.gz | tar -xzv
+    curl  http://$host:8080/plugins/fuel-plugin-ovs-1.0/repositories/ubuntu/ovs-nsh-dpdk.tar.gz | tar -xzv
     dpkg -i openvswitch-datapath-dkms_2.5.90-1.nsh_all.deb
     dpkg -i openvswitch-common_2.5.90-1.nsh_amd64.deb
     dpkg -i openvswitch-switch_2.5.90-1.nsh_amd64.deb
@@ -25,7 +25,7 @@ then
         dpkg -i openvswitch-switch-dpdk_2.5.90-1.nsh_amd64.deb
     fi
 else
-    curl  http://$host:8080/plugins/fuel-plugin-ovs-0.9/repositories/ubuntu/ovs-dpdk.tar.gz | tar -xzv
+    curl  http://$host:8080/plugins/fuel-plugin-ovs-1.0/repositories/ubuntu/ovs-dpdk.tar.gz | tar -xzv
     dpkg -i openvswitch-datapath-dkms_2.5.90-1_all.deb
     dpkg -i openvswitch-common_2.5.90-1_amd64.deb
     dpkg -i openvswitch-switch_2.5.90-1_amd64.deb
