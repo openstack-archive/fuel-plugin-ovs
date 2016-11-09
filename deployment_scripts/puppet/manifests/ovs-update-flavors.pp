a8f78d202d502f0bc51d12d88b39905a8e8278e8
Romanos Skiadas <rski@intracom-telecom.com>
2016-11-09 11:10:58 +0200

    Fix for installing ovs on controller, update urls
    $install_on_controller was being compared to a quoted true, and since
    true != 'true', the expression always evaluated to false, regardless of
    whether the feature was enabled.
    Also update the urls from where the scripts are pulled to point to 1.0
    version of the plugin.
    
    Change-Id: Ifa961240d1ccd53e56b93d5afae7da0df26c1e34
    Signed-off-by: Romanos Skiadas <rski@intracom-telecom.com>

--- deployment_scripts/puppet/manifests/ovs-update-flavors.pp
+++ deployment_scripts/puppet/manifests/ovs-update-flavors.pp
@@ -3,8 +3,8 @@ $master_ip      = hiera('master_ip')
 
 if $operatingsystem == 'Ubuntu' {
     exec { 'update flavors':
-        command => "curl http://${master_ip}:8080/plugins/fuel-plugin-ovs-0.9/deployment_scripts/update_flavors.sh | bash -s",
-        path   => "/usr/bin:/usr/sbin:/bin:/sbin",
+        command => "curl http://${master_ip}:8080/plugins/fuel-plugin-ovs-1.0/deployment_scripts/update_flavors.sh | bash -s",
+        path    => '/usr/bin:/usr/sbin:/bin:/sbin',
     }
 } elsif $operatingsystem == 'CentOS' {
 }
