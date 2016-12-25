199e239e85803ad6bfe21c60e2f2d4f24e9cf4b4
Guo Ruijing <ruijing.guo@intel.com>
2016-12-25 17:54:25 -0500

    ovs rebase and add ovs with NSH/DPDK
    1. rebase to ovs commit 92043ab8ffd449dfd50c3e716d6db06d04af70d7
    2. add DPDK for ovs with NSH
    3. clean up later for easy code review
       a) change openvswitch-dpdk_2.5.90 to openvswitch_2.6-dpdk_16.07
       a) delete dpdk_2.2.0
       b) delete openvswitch-nsh-dpdk_2.5.90
    
    Change-Id: I4d8b16a1c2517dba2b9356e6436885b65ac4b307
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- ovs_build/build-ovs-nsh-dpdk.sh
+++ ovs_build/build-ovs-nsh-dpdk.sh
@@ -3,11 +3,10 @@
 set -eux
 
 OVS_COMMIT=f4b0e64cffb4777ff03d48621c3eadcf1d8c19f3
-URL_OVS=https://github.com/openvswitch/ovs.git
 OVS_VER=${OVS_VER:-2.6.1}
-BUILD_HOME=$HOME/nsh
 BUILD_DEST=${BUILD_DEST:-/deb}
-DIR="$(dirname `readlink -f $0`)"
+BUILD_SRC="$(dirname `readlink -f $0`)"
+BUILD_HOME=${BUILD_HOME:-/tmp/ovs-dpdk}
 
 export DEB_BUILD_OPTIONS='parallel=8 nocheck'
 
@@ -16,38 +15,82 @@ sudo apt-get -y install devscripts dpkg-dev git wget
 rm -rf ${BUILD_HOME}; mkdir -p ${BUILD_HOME}
 
 cd ${BUILD_HOME}
+wget -c http://fast.dpdk.org/rel/dpdk-16.07.tar.xz
+xz -d dpdk-16.07.tar.xz; tar xvf dpdk-16.07.tar
+cd dpdk-16.07
+cp -r ${BUILD_SRC}/dpdk_16.07/debian .
 
 # copy from debian/control
-sudo apt-get install -y \
-               graphviz \
-               autoconf \
+sudo apt-get install -y debhelper \
+               dh-python \
+               dh-systemd \
+               doxygen  \
+               graphviz  \
+               inkscape  \
+               libcap-dev  \
+               libpcap-dev  \
+               libxen-dev  \
+               libxenstore3.0  \
+               python  \
+               python-sphinx  \
+               texlive-fonts-recommended  \
+               texlive-latex-extra
+debian/rules build; fakeroot debian/rules binary
+
+cd ${BUILD_HOME}
+sudo dpkg -i *.deb
+apt-get download libxenstore3.0
+
+# copy from debian/control
+sudo apt-get install -y autoconf \
                automake \
                bzip2 \
                debhelper \
                dh-autoreconf \
+               dh-systemd \
+               graphviz \
+               libdpdk-dev \
+               libfuse-dev \
                libssl-dev \
                libtool \
                openssl \
                procps \
                python-all \
+               python-qt4 \
                python-twisted-conch \
                python-zopeinterface \
                python-six
 
 git clone https://github.com/openvswitch/ovs.git
-cd ovs; git checkout ${OVS_COMMIT}
-PATCHES=$(cd ${DIR}/ovs_nsh_patches/v2.6.1/; echo *patch)
+cd ovs; git checkout ${OVS_COMMIT}; rm -rf .git
+PATCHES=$(cd ${BUILD_SRC}/ovs_nsh_patches/v2.6.1/; echo *patch)
 for patch in ${PATCHES}
 do
-    patch -p1 < ${DIR}/ovs_nsh_patches/v2.6.1/${patch}
+    patch -p1 < ${BUILD_SRC}/ovs_nsh_patches/v2.6.1/${patch}
 done
+cd ${BUILD_HOME}; cp -r ovs ovs-dpdk
 
-# build ovs
+cd ovs-dpdk
+cp -r ${BUILD_SRC}/openvswitch-dpdk_2.5.90/debian .
+cat << EOF > debian/changelog
+openvswitch-dpdk (${OVS_VER}-1.nsh) unstable; urgency=low
+  [ Open vSwitch team ]
+  * support NSH & DPDK 16.07
+
+ -- Open vSwitch team <dev@openvswitch.org>  $(date --rfc-2822)
+EOF
+
+debian/rules build; fakeroot debian/rules binary
+
 cd ${BUILD_HOME}/ovs
 cat << EOF > debian/changelog
 openvswitch (${OVS_VER}-1.nsh) unstable; urgency=low
-  * Support NSH
+  [ Open vSwitch team ]
+  * support NSH
+
  -- Open vSwitch team <dev@openvswitch.org>  $(date --rfc-2822)
 EOF
 debian/rules build; fakeroot debian/rules binary
-cp ${BUILD_HOME}/*.deb ${BUILD_DEST}
+
+cp -r ${BUILD_HOME}/*.deb ${BUILD_DEST}
+rm -rf ${BUILD_HOME}
