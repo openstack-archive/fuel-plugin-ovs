98bbf84f62a895c635948d39724c4ba3122c2fa4
Guo Ruijing <ruijing.guo@intel.com>
2017-02-02 15:26:20 -0500

    Clean up and Remove DPDK from ovs with dpdk
    Change-Id: If3d4e8d9fa00077fe21959ad61eed9b423bc1ae8
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- deployment_scripts/puppet/manifests/ovs-install.pp
+++ deployment_scripts/puppet/manifests/ovs-install.pp
@@ -2,18 +2,14 @@ notice('MODULAR: ovs-install.pp')
 # get options
 $master_ip      = hiera('master_ip')
 $ovs_settings   = hiera('fuel-plugin-ovs')
-$dpdk           = hiera_hash('dpdk', {})
 
 $install_nsh    = $ovs_settings['install_nsh']
 $install_dpdk   = $ovs_settings['install_dpdk']
-$install_on_controller = $ovs_settings['install_on_controller']
-$ovs_socket_mem = join(pick($dpdk['ovs_socket_mem'], []), ',')
-$ovs_pmd_core_mask = $dpdk['ovs_pmd_core_mask']
 
 if $operatingsystem == 'Ubuntu' {
   if (!roles_include(['primary-controller', 'controller'])) or $install_on_controller {
       exec { 'install ovs_nsh_dpdk':
-          command => "curl http://${master_ip}:8080/plugins/fuel-plugin-ovs-1.0/deployment_scripts/install.sh | bash -s ${master_ip} ${install_nsh} ${install_dpdk} ${ovs_socket_mem} ${ovs_pmd_core_mask}",
+          command => "/etc/fuel/plugins/fuel-plugin-ovs-1.0/install.sh ${master_ip} ${install_nsh} ${install_dpdk}",
           path    => '/usr/bin:/usr/sbin:/bin:/sbin',
       }
     }
